28040f0f35f37561dd9ca3c7fc728207152a8b6e
Osama KHAIT <105494900+OsamaKhait@users.noreply.github.com>
2023-06-18 23:44:53 +0200

    Delete base.php

--- base.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $bd = new PDO ( "mysql:host=localhost;dbname=thales", "root", "");
-    $bd->exec ('SET NAMES utf8') ;
-}
-catch (Exception $e) {
-    die ("Erreur: Connexion à la base impossible");
-}
-?>
